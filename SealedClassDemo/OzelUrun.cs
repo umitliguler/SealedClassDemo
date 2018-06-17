3731d64b5c3f9f024bec11dd33cfd58c100f15b2
umitliguler <info.umitguler@gmail.com>
2018-06-17 12:34:31 +0300

    first

---
+++ SealedClassDemo/OzelUrun.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SealedClassDemo
+{
+    sealed class OzelUrun
+    {
+        public int UrunKodu { get; set; }
+    }
+}
